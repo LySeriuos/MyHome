df888ab18ea1284f2fe748e1d8def8fb497de239
LySeriuos <drungilas.p@gmail.com>
2023-08-13 00:02:22 +0200

    added classes for device warranty

--- DevicesProfile.cs
+++ DevicesProfile.cs
@@ -1,28 +1,80 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace My_Home
+namespace Device_Profile
 {
+	
     internal class DevicesProfile
     {
+		
 		private string _deviceName;
-
 		public string DeviceName
 		{
 			get { return _deviceName; }
 			set { _deviceName = value; }
 		}
 
-		private DeviceType _deviceType;
-
-		public enum DeviceType
+		private SoundDevicesType _soundDeviceType;
+		public SoundDevicesType SoundDevicesType
 		{
-			get { return _deviceType; }
-			set { _deviceType = value; }
+			get { return _soundDeviceType; }
+			set { _soundDeviceType = value; }
 		}
+
+		private KitchenDevicesType _kitchenDevicesType;
+		public KitchenDevicesType KitchenDevicesType
+		{
+			get { return _kitchenDevicesType; }
+			set { _kitchenDevicesType = value; }
+		}
+
+		private WashingCleaningDevicesType _washingCleaningDevicesType;
+		public WashingCleaningDevicesType WashingCleaningDevicesType
+		{
+			get { return _washingCleaningDevicesType; }
+			set { _washingCleaningDevicesType = value; }
+		}
+
+		private VisualDevicesType _visualDeviceType;
+		public VisualDevicesType VisualDevicesType
+		{
+			get { return _visualDeviceType; }
+			set { _visualDeviceType = value; }
+		}
+
+		private SmartDevicesType _smartDevicesType;
+		public SmartDevicesType SmartDevicesType
+		{
+			get { return _smartDevicesType; }
+			set { _smartDevicesType = value; }
+		}
+
+		private string _deviceModelNumber;
+
+		public string DeviceModelNumber
+		{
+			get { return _deviceModelNumber; }
+			set { _deviceModelNumber = value; }
+		}
+
+		private string myVar;
+
+		public string MyProperty
+		{
+			get { return myVar; }
+			set { myVar = value; }
+		}
+
+
+
+
+
+
 
 
 
--- SmartDevicesType.cs
+++ SmartDevicesType.cs
@@ -4,13 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace My_Home
+namespace Device_Profile
 {
     public enum SmartDevicesType
     {
         MobilePhone,       
         TabletComputer,
         Computer,
-        Laptop
+        Laptop,
+        SmartWatch,
+        Other
     }
 }
