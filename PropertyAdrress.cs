e880ba4ae3003ac9a30e8b74736abfee331a8469
LySeriuos <drungilas.p@gmail.com>
2023-08-13 12:30:36 +0200

    attached DevicesProfile class to PropertyAdrress class

--- PropertyAdrress.cs
+++ PropertyAdrress.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Device_Profile;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,53 @@ namespace My_Home
 {
     internal class PropertyAdrress
     {
-    }
+		private string _streetName;
+
+		public string StreetName
+		{
+			get { return _streetName; }
+			set { _streetName = value; }
+		}
+
+		private int _houseNumber;
+
+		public int HouseNumber
+		{
+			get { return _houseNumber; }
+			set { _houseNumber = value; }
+		}
+
+		private string _houseNumberExtension;
+
+		public string HouseNumberExtension
+		{
+			get { return _houseNumberExtension; }
+			set { _houseNumberExtension = value; }
+		}
+
+		private string _city;
+
+		public string City
+		{
+			get { return _city; }
+			set { _city = value; }
+		}
+
+		private string _country;
+
+		public string Country
+		{
+			get { return _country; }
+			set { _country = value; }
+		}
+
+		private DevicesProfile _devicesProfile;
+
+		public DevicesProfile DevicesProfile
+		{
+			get { return _devicesProfile; }
+			set { _devicesProfile = value; }
+		}
+
+	}
 }
