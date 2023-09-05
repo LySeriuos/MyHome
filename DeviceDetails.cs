ca0855ad35bc346637e7dd1604d244f4b81e93ba
LySeriuos <drungilas.p@gmail.com>
2023-09-05 11:27:02 +0200

    edited class device details and removed unnecessary methods, moved functionallity to switch case

--- DeviceDetails.cs
+++ DeviceDetails.cs
@@ -9,133 +9,49 @@ namespace MyHome
 {
     public static class DeviceDetails
     {
-        public static List<string> GetAudio()
+       
+        public static List<string> GetDetails(DeviceType deviceType)
         {
-            var audioList = new List<string>() { "Speaker", "SoundBar", "Portable Speaker", "Amplifier" };
+            switch (deviceType)
+            {
+                case DeviceType.Audio:
+                    return new List<string>() { "Speaker", "SoundBar", "Portable Speaker", "Amplifier" };
 
-            return audioList;
+                case DeviceType.Video:
+                    return new List<string>() { "TV", "Projector", "Monitor", "Camera", };
+
+                case DeviceType.Computer:
+                    return new List<string>() { "Computer", "Laptop", "Portable Hard Disk", "Tablet PC", "Home Assistant" };
+
+                case DeviceType.MobileDevice:
+                    return new List<string>() { "Phone", "Ipad", "Watch" };
+
+                case DeviceType.Kitchen:
+                    return new List<string>() { "Mixer", "Oven", "Microwave", "Airfryer", "Dish Washer" };
+
+                case DeviceType.Bathroom:
+                    return new List<string>() { "Hairdryer", "Shaving Machine", "Skin Lazer" };
+
+                case DeviceType.Garden:
+                    return new List<string>() { "Grass Trimmer", "Grass Cuting Robot", "Watering System" };
+
+                case DeviceType.Cleaning:
+                    return new List<string>() { "Washing Machine", "Drying Machine", "Robot Vacuum Cleaner" };
+                        
+                case DeviceType.Security:
+                    return new List<string>() { "Security Cameras", "Security Sensors" };
+
+                case DeviceType.Other:
+                    return new List<string>() { "Navigation", "Home Lighting" };                    
+
+                case DeviceType.MultiDevice:
+                    return new List<string>() { "Something special" };
+                default:
+                    return new List<string>() { "Not Sorted Device" };
+
+            }
+            
         }
-
-        public static List<string> GetVideo()
-        {
-            var videoList = new List<string>() { "TV", "Projector", "Monitor", "Camera", };
-
-            return videoList;
-        }
-
-        public static List<string> GetComputer()
-        {
-            var computerList = new List<string>() { "Computer", "Laptop", "Portable Hard Disk", "Tablet PC", "Home Assistant" };
-
-            return computerList;
-        }
-
-        public static List<string> GetMobileDevice()
-        {
-            var mobileDeviceList = new List<string>() { "Phone", "Ipad", "Watch" };
-
-            return mobileDeviceList;
-        }
-
-        public static List<string> GetKitchen()
-        {
-            var kitchenList = new List<string>() { "Mixer", "Oven", "Microwave", "Airfryer", "Dish Washer" };
-
-            return kitchenList;
-        }
-
-        public static List<string> GetBathroom()
-        {
-            var bathroomList = new List<string>() { "Hairdryer", "Shaving Machine", "Skin Lazer" };
-
-            return bathroomList;
-        }
-
-        public static List<string> GetGarden()
-        {
-            var gardenList = new List<string>() { "Grass Trimmer", "Grass Cuting Robot", "Watering System" };
-
-            return gardenList;
-        }
-
-        public static List<string> GetCleaning()
-        {
-            var cleaningList = new List<string>() { "Washing Machine", "Drying Machine", "Robot Vacuum Cleaner" };
-
-            return cleaningList;
-        }
-
-        public static List<string> GetSecurity()
-        {
-            var securityList = new List<string>() { "Security Cameras", "Security Sensors" };
-
-            return securityList;
-        }
-
-        public static List<string> GetOtherDevices()
-        {
-            var otherDevicesList = new List<string>() { "Navigation", "Home Lighting" };
-
-            return otherDevicesList;
-        }
-
-        public static List<string> GetMultiDevices()
-        {
-            var multiDevicesList = new List<string>() { "Something special" };
-
-            return multiDevicesList;
-        }
-
-        //private static List<String> GetDetails(DeviceType deviceType)
-        //{
-        //    switch (deviceType)
-        //    {
-        //        case DeviceType.Audio:
-        //            GetAudio();
-        //            break;
-
-        //        case DeviceType.Video:
-        //            GetVideo();
-        //            break;
-
-        //        case DeviceType.Computer:
-        //            GetComputer();
-        //            break;
-
-        //        case DeviceType.MobileDevice:
-        //            GetMobileDevice();
-        //            break;
-
-        //        case DeviceType.Kitchen:
-        //            GetKitchen();
-        //            break;
-
-        //        case DeviceType.Bathroom:
-        //            GetBathroom();
-        //            break;
-
-        //        case DeviceType.Garden:
-        //            GetGarden();
-        //            break;
-
-        //        case DeviceType.Cleaning:
-        //            GetCleaning();
-        //            break;
-
-        //        case DeviceType.Security:
-        //            GetSecurity();
-        //            break;
-
-        //        case DeviceType.Other:
-        //            return new List<string>() { "Navigation", "Home Lighting" };
-        //            break;
-
-        //        case DeviceType.MultiDevice:
-        //            GetMultiDevices();
-        //            break;
-
-        //    }
-        //}
     }
 }
 
