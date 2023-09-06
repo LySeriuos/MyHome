04e732fd724cfeb6a3885e6606ef3b0ef9c45130
LySeriuos <drungilas.p@gmail.com>
2023-09-06 20:41:09 +0200

    cleaned the main class

--- Program.cs
+++ Program.cs
@@ -8,16 +8,12 @@ namespace My_Home
     {
         static void Main(string[] args)
         {
-
-            
             List<UserProfile> list = TestData.User();
-            List<DevicesProfile> devicesClosestToTheEndList = Logic.DevicesAndWarranties(list[1], 1500);
+            List<DevicesProfile> devicesClosestToTheEndList = Logic.DevicesAndWarranties(list[1], 180);
             foreach (DevicesProfile d in devicesClosestToTheEndList)
             {
                 Console.WriteLine(d);
             }
-
-            List<RealEstate> realEstates = Logic.RealEstates(list[1]);
 
             DeviceType deviceType;
             List<DevicesProfile> devices = Logic.GetAllUserDevices(list[1]);
@@ -26,13 +22,8 @@ namespace My_Home
                 deviceType = device.DeviceType;
                 DeviceWarranty deviceWarranty = device.DeviceWarranty;
                 Shop shop = deviceWarranty.Shop;
-                //Console.WriteLine(shop.ShopWebAddress.ToString());
             }
             DeviceWarranty warranty = Logic.GetUserDevicesWarranties(list[0]);
-            List<DeviceType> deviceDetails = Logic.GetSubcategoryList(devices);
-
-
-        //    List<string> strings1 = DeviceDetails.GetDetails(deviceType);
 
 
 
