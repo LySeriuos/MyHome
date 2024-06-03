8b4fe7fa64a830e7bd1197840799617ebb8f5b77
LySeriuos <drungilas.p@gmail.com>
2024-06-03 23:36:20 +0200

    created component for Edit Shop Modal, not finished!

--- MyHomeBlazorApp/BlazorData/DataService.cs
+++ MyHomeBlazorApp/BlazorData/DataService.cs
@@ -82,6 +82,7 @@ namespace MyHomeBlazorApp.BlazorData
                         break;
                     }
                 }
+                
             }
             return currentRealEstateTest.RealEstateID;
         }
