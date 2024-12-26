796b23ce4345a22728a0dfcc8b9430e10a38c51a
LySeriuos <drungilas.p@gmail.com>
2024-12-26 13:40:06 +0100

    fixed issue with changed page name, there was not possible to make active qrCode because of worng page name.

--- MyHome/Logic.cs
+++ MyHome/Logic.cs
@@ -144,9 +144,9 @@ namespace MyHome
         public static void CreateQrCodeLinkToDevice(string deviceID, string userID, string savedQrCodeLink)
         {
             // This is used for test on local environment, new Url every time visual studio started. 
-            Url absoluteUrl = new Url("https://rwgl97zd-7211.euw.devtunnels.ms");
+            Url absoluteUrl = new Url("https://xqgznzvc-7211.euw.devtunnels.ms");
             //Url qrCodeLink = new Url($"{absoluteUrl}mdetails/{userID}/{deviceID}");
-            Url qrCodeUrl = new Url($"{absoluteUrl}/mdetails/{userID}/{deviceID}");
+            Url qrCodeUrl = new Url($"{absoluteUrl}/mobileDeviceInfo/{userID}/{deviceID}");
             string generatedQrCodeLink = qrCodeUrl.ToString();
             QrCodeGenerator(generatedQrCodeLink, savedQrCodeLink);
         }
