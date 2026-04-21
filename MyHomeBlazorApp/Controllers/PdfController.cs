2e1f72cd5f0a926956b41187bc56e9aef824f8b6
LySeriuos <drungilas.p@gmail.com>
2026-04-21 11:57:44 +0200

    created empty api controller class for pdf. Installed QuestPdf nuget package

---
+++ MyHomeBlazorApp/Controllers/PdfController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyHomeBlazorApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PdfController : ControllerBase
+    {
+    }
+}
